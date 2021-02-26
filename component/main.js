5057695091823fdd9d372a5bfa99fa9626b21df0
wangyan-workspace <2107014530@qq.com>
2021-02-26 21:05:51 +0800

    过滤器，自定义插件，组件

---
+++ component/main.js
@@ -0,0 +1,11 @@
+Vue.component('app-main',{
+    template: `
+        <div class="main">
+            <ul>
+                <li>用户管理</li>
+                <li>账单管理</li>
+                <li>供应商管理</li>
+            </ul>
+        </div>
+    `
+})
